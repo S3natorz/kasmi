fd0e2ab84c976bee72361fe083afc45887a94854
S3natorz <hamidfadhilah@unisba.ac.id>
2025-11-29 22:40:08 +0700

    fix: Add explicit PWA icon links in head for iOS/Android compatibility

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -73,6 +73,24 @@ const RootLayout = async (props: ChildrenType & { params: Promise<{ lang: string
   return (
     <TranslationWrapper headersList={headersList} lang={lang}>
       <html id='__next' lang={lang} dir={direction} suppressHydrationWarning>
+        <head>
+          {/* PWA Icons - Explicit for better compatibility */}
+          <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
+          <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
+          <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
+          <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon-180x180.png' />
+          <link rel='apple-touch-icon' sizes='167x167' href='/icons/apple-touch-icon-167x167.png' />
+          <link rel='apple-touch-icon' sizes='152x152' href='/icons/apple-touch-icon-152x152.png' />
+          <link rel='apple-touch-icon' sizes='120x120' href='/icons/apple-touch-icon-120x120.png' />
+          <link rel='manifest' href='/manifest.json' />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+          <meta name='apple-mobile-web-app-title' content='Kasmi' />
+          <meta name='mobile-web-app-capable' content='yes' />
+          <meta name='theme-color' content='#7367f0' />
+          <meta name='msapplication-TileColor' content='#7367f0' />
+          <meta name='msapplication-TileImage' content='/icons/icon-144x144.png' />
+        </head>
         <body className='flex is-full min-bs-full flex-auto flex-col'>
           <InitColorSchemeScript attribute='data' defaultMode={systemMode} />
           {children}
